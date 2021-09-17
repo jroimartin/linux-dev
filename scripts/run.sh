1dd75289b80543d901955f400f4c00d00ee7f81f
Roi Martin <jroi.martin@gmail.com>
2021-09-18 01:32:44 +0200

    scripts: add run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+set -eu
+
+BUILD_DIR=${BUILD_DIR:-"$PWD/build"}
+
+qemu-system-x86_64 \
+	-nographic \
+	-kernel "${BUILD_DIR}/bzImage" \
+	-initrd "${BUILD_DIR}/initramfs.cpio.gz" \
+	-append 'console=ttyS0'
