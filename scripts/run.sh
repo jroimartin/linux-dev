34ef2dbb0dae59a0b2b71ac121aec82e8c7aab5c
Roi Martin <jroi.martin@gmail.com>
2021-09-19 11:45:37 +0200

    scripts: run.sh: launch qemu with -nodefaults
    Signed-off-by: Roi Martin <jroi.martin@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,8 @@ BUILD_DIR=${BUILD_DIR:-"$PWD/build"}
 
 qemu-system-x86_64 \
 	-nographic \
+	-nodefaults \
+	-serial 'mon:stdio' \
 	-kernel "${BUILD_DIR}/bzImage" \
 	-initrd "${BUILD_DIR}/initramfs.cpio.gz" \
 	-append 'console=ttyS0'
